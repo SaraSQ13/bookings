99702227df322d7588e6b3554f66fde30afc6d02
Sara <sara.s.q@hotmail.com>
2022-12-09 13:07:28 +0100

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const db = require("./db/db");
+// const db = require("./db/db");
 const router = require("./router");
 const cors = require('cors');
 
